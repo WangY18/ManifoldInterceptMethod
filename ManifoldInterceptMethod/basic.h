26ef498ede35ee8f0cf4bc5ba1d4ab8501333ed5
Wang <8198927+wangyuna18@user.noreply.gitee.com>
2024-11-06 21:23:58 +0800

    1

---
+++ ManifoldInterceptMethod/basic.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct arc {
+	int constriant;
+	bool sign; // true: +; false: -.
+	int tangent; // 0: arc; k>0: tangent marker
+};
---
+++ ManifoldInterceptMethod/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "demo.h"
+using namespace std;
+
+int main() {
+	cout << "Hello, World!" << endl;
+	cout << "This is the main file for the Manifold Intercept Method." << endl;
+	return 0;
+}
